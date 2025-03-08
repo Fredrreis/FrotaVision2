6897db27a717b3b9661c98681687d3215e18cf90
Fred <frederico9630@gmail.com>
2025-03-08 16:35:56 -0300

    Nova formatação e design para o componente Servico

--- src/app/home/components/servico/servico.tsx
+++ src/app/home/components/servico/servico.tsx
@@ -5,7 +5,9 @@ import "./servico.css";
 import { Box, Typography, Button } from "@mui/material";
 import Image from "next/image";
 import { motion } from "framer-motion";
-import ServicoImage from "../../../img/two-guys-talking-about-work-work-garage-near-truck-transfer-documents-with-goods.png";
+import ServicoImage3 from "../../../img/two-guys-talking-about-work-work-garage-near-truck-transfer-documents-with-goods.png";
+import ServicoImage2 from "../../../img/marcin-jozwiak-kGoPcmpPT7c-unsplash.jpg"
+import ServicoImage1 from "../../../img/seb-creativo-3jG-UM8IZ40-unsplash.jpg"
 import LocalShippingIcon from "@mui/icons-material/LocalShipping";
 import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
 import MoreTimeIcon from "@mui/icons-material/MoreTime";
@@ -18,17 +20,22 @@ const containerVariants = {
 
 const servicos = [
   {
-    icone: <CurrencyExchangeIcon className="servico-icon" />, 
+    icone: CurrencyExchangeIcon, 
     titulo: "Manutenção Preventiva Inteligente",
     descricao: "Receba alertas automáticos sobre o momento ideal para realizar manutenções preventivas, evitando falhas inesperadas e reduzindo custos com reparos emergenciais.",
   },
   {
-    icone: <LocalShippingIcon className="servico-icon" />, 
+    img: ServicoImage1,
+  },
+  {
+    img: ServicoImage2,
+    icone: LocalShippingIcon, 
     titulo: "Monitoramento Eficiente",
     descricao: "Tenha total controle sobre os veículos da frota, acompanhando suas condições e garantindo que cada caminhão esteja sempre pronto para rodar com segurança e eficiência.",
   },
   {
-    icone: <MoreTimeIcon className="servico-icon" />, 
+    img: ServicoImage3,
+    icone: MoreTimeIcon,
     titulo: "Agilidade na Tomada de Decisão",
     descricao: "Com uma plataforma intuitiva, você acessa rapidamente as informações essenciais, agiliza o planejamento de manutenção e melhora a produtividade da frota.",
   },
@@ -42,35 +49,43 @@ export default function Servico() {
       initial="hidden"
       animate="visible"
     >
-      <Typography variant="h5" className="servico-title">
-        Por que escolher o FrotaVision?
-      </Typography>
-
-      <Box className="servico-image">
-        <Image
-          src={ServicoImage}
-          className="servico-image2"
-          alt="Imagem de serviço"
-          layout="responsive"
-        />
-      </Box>
-
       <Box className="servico-content">
         {servicos.map((servico, index) => (
-          <Box key={index} className="servico-item">
-            <Box className="servico-subtitle-container">
-              {servico.icone}
-              <Typography variant="subtitle1" className="servico-subtitle">
-                {servico.titulo}
-              </Typography>
-            </Box>
-            <Typography variant="body2" className="servico-text">
-              {servico.descricao}
+        <Box
+          key={index}
+          className={`servico-item ${servico.titulo && servico.img ? "dark-overlay" : ""}`}
+          sx={{
+            backgroundImage: servico.img ? `url(${servico.img.src})` : "none",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            color: servico.img ? "white" : "black",
+            position: "relative",
+            padding: servico.titulo === "Agilidade na Tomada de Decisão" ? "0 10vw 0 6vw" : "0 6vw 0 10vw",
+          }}
+        >
+          {!servico.img && (
+            <Typography variant="h6" className="servico-title">
+              SERVIÇOS
+            </Typography>
+          )}
+
+          <Box className="servico-subtitle-container">
+            {servico.icone &&
+              React.createElement(servico.icone, {
+                className: "servico-icon",
+                sx: { color: servico.img ? "white" : "#1b3562" },
+              })}
+            <Typography variant="subtitle1" className="servico-subtitle">
+              {servico.titulo}
             </Typography>
           </Box>
+          <Typography variant="body2" className="servico-text">
+            {servico.descricao}
+          </Typography>
+        </Box>
         ))}
       </Box>
-
       <KeyboardDoubleArrowDownIcon className="arrow-icon" />
       <Box className="servico-button-container">
         <Button variant="outlined" className="servico-button">
